282de5e5256f1717b2f2cd944901b1405e2e7468
Andrew Amis <atamiser@gmail.com>
2018-07-29 17:16:02 -0700

    Made lisp data printing cleaner.

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,8 @@
 use std::rc::Rc;
+use std::fmt;
 
 /// Omni-datatype. Represents both data and code for the lisp VM.
-#[derive(Debug, Clone)]
+#[derive(Clone, PartialEq)]
 pub enum Lisp {
     /// Represents a single u32 number.
     Num(u32),
@@ -11,8 +12,18 @@ pub enum Lisp {
     List(Rc<Vec<Rc<Lisp>>>),
 }
 
+impl fmt::Debug for Lisp {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Lisp::Num(i) => write!(f, "{:?}", i),
+            Lisp::Op(o) => write!(f, "{:?}", o),
+            Lisp::List(l) => write!(f, "{:?}", l),
+        }
+    }
+}
+
 /// Enum of basic operations.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Op {
     /// Represents addition. Currently variadic.
     Add,
