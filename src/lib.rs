637c57007e78a426a8cc128a7762268a0ffafe69
Andrew Amis <atamiser@gmail.com>
2018-08-10 15:34:06 -0700

    Refactored VM operations.

--- src/lib.rs
+++ src/lib.rs
@@ -143,88 +143,114 @@ pub mod vm {
             };
 
             match op {
-                Op::Lit(l) => {
-                    self.stack.push(l);
-                    ()
-                }
-                Op::Return => {
-                    self.frames.pop().ok_or("Attempted to return on empty stack")?;
-                    ()
-                }
-                Op::Call => {
-                    let a = self
-                        .stack
-                        .pop()
-                        .ok_or("Attempted to pop data stack for jump")?;
-                    match a {
-                        Literal::Address(a) => {
-                            self.frames.push(a);
-                            ()
-                        }
-                        _ => return Err("attempted to jump to non-address".into()),
-                    };
-                    ()
-                }
-                Op::Jump => {
-                    let address = self
-                        .stack
-                        .pop()
-                        .ok_or("Attempted to pop stack for address")?
-                        .ensure_address()?;
-                    self.jump(address)?;
-                }
-                Op::JumpCond => {
-                    let cond = self
-                        .stack
-                        .pop()
-                        .ok_or("Attempted to pop stack for conditional for if zero")?
-                        .ensure_bool()?;
-                    let then = self
-                        .stack
-                        .pop()
-                        .ok_or("Attepmted to pop stack for address for if true")?
-                        .ensure_address()?;
-                    let els = self
-                        .stack
-                        .pop()
-                        .ok_or("Attepmted to pop stack for address for if false")?
-                        .ensure_address()?;
+                Op::Lit(l) => self.op_lit(l).chain_err(|| "Executing operation literal"),
+                Op::Return => self.op_return().chain_err(|| "Executing operation return"),
+                Op::Call => self.op_call().chain_err(|| "Executing operation call"),
+                Op::Jump => self.op_jump().chain_err(|| "Executing operation jump"),
+                Op::JumpCond => self.op_jumpcond().chain_err(|| "Executing operation jumpcond"),
+                Op::Load => self.op_load().chain_err(|| "Executing operation load"),
+                Op::Store => self.op_store().chain_err(|| "Executing operation store"),
+                Op::PushEnv => self.op_pushenv().chain_err(|| "Executing operation pushenv"),
+                Op::PopEnv => self.op_popenv().chain_err(|| "Executing operation popenv"),
+                Op::Dup => self.op_dup().chain_err(|| "Executing operation dup"),
+            }
+        }
 
-                    if cond {
-                        self.jump(then)?;
-                    } else {
-                        self.jump(els)?;
-                    }
-                }
-                Op::PushEnv => {
-                    self.environment.push();
-                },
-                Op::PopEnv => {
-                    self.environment.pop()?;
-                },
-                Op::Load => {
-                    let keyword = self.stack.pop()
-                        .ok_or("Attempted to pop stack for keyword for load")?
-                        .ensure_keyword()?;
-                    let mut val = self.environment.get(&keyword)?;
-                    let mut val = Rc::make_mut(&mut val);
-                    self.stack.push(val.clone())
-                },
-                Op::Store => {
-                    let keyword = self.stack.pop()
-                        .ok_or("Attempted to pop stack for keyword for store")?
-                        .ensure_keyword()?;
-                    let value = self.stack.pop()
-                        .ok_or("Attempted to pop stack for value for store")?;
+        fn op_lit(&mut self, l: data::Literal) -> Result<()> {
+            self.stack.push(l);
+            Ok(())
+        }
 
-                    self.environment.put(keyword, Rc::new(value));
-                }
-                Op::Dup => {
-                    let v = self.stack.last().ok_or("Attmempted to dup empty stack")?.clone();
-                    self.stack.push(v);
-                }
-            };
+        fn op_return(&mut self) -> Result<()> {
+            self.frames.pop().ok_or("Attempted to return on empty stack")?;
+            Ok(())
+        }
 
+        fn op_call(&mut self) -> Result<()> {
+            let a = self
+                .stack
+                .pop()
+                .ok_or("Attempted to pop data stack for jump")?;
+
+            if let Literal::Address(addr) = a {
+                self.frames.push(addr);
+                Ok(())
+            } else {
+                Err("attempted to jump to non-address".into())
+            }
+        }
+
+        fn op_jump(&mut self) -> Result<()> {
+            let address = self
+                .stack
+                .pop()
+                .ok_or("Attempted to pop stack for address")?
+                .ensure_address()?;
+
+            self.jump(address)
+        }
+
+        fn op_jumpcond(&mut self) -> Result<()> {
+            let cond = self
+                .stack
+                .pop()
+                .ok_or("Attempted to pop stack for conditional for if zero")?
+                .ensure_bool()?;
+
+            let then = self
+                .stack
+                .pop()
+                .ok_or("Attepmted to pop stack for address for if true")?
+                .ensure_address()?;
+
+            let els = self
+                .stack
+                .pop()
+                .ok_or("Attepmted to pop stack for address for if false")?
+                .ensure_address()?;
+
+            if cond {
+                self.jump(then)
+            } else {
+                self.jump(els)
+            }
+        }
+
+        fn op_load(&mut self) -> Result<()> {
+            let keyword = self.stack.pop()
+                .ok_or("Attempted to pop stack for keyword for load")?
+                .ensure_keyword()?;
+
+            let mut val = self.environment.get(&keyword)?;
+
+            let mut val = Rc::make_mut(&mut val);
+
+            self.stack.push(val.clone());
+            Ok(())
+        }
+
+        fn op_store(&mut self) -> Result<()> {
+            let keyword = self.stack.pop()
+                .ok_or("Attempted to pop stack for keyword for store")?
+                .ensure_keyword()?;
+            let value = self.stack.pop()
+                .ok_or("Attempted to pop stack for value for store")?;
+
+            self.environment.put(keyword, Rc::new(value));
+
+            Ok(())
+        }
+        fn op_pushenv(&mut self) -> Result<()> {
+            self.environment.push();
+            Ok(())
+        }
+        fn op_popenv(&mut self) -> Result<()> {
+            self.environment.pop()?;
+            Ok(())
+        }
+        fn op_dup(&mut self) -> Result<()> {
+            let v = self.stack.last().ok_or("Attmempted to dup empty stack")?.clone();
+            self.stack.push(v);
             Ok(())
         }
     }
