6962016be5718fd3f6c1d5e6713ce98f910170cf
Andrew Amis <atamiser@gmail.com>
2018-11-26 14:46:10 -0800

    Wrote visitors for the LocalAST family of structs.

--- isl/src/ast/passes/local.rs
+++ isl/src/ast/passes/local.rs
@@ -28,11 +28,6 @@ pub enum LocalAST {
         body: Rc<LocalAST>,
     },
     Do(Vec<LocalAST>),
-    Lambda {
-        args: Vec<Keyword>,
-        body: Rc<LocalAST>,
-    },
-    Local(usize),
     GlobalVar(Keyword),
     LocalVar(usize),
     Application {
@@ -226,6 +221,194 @@ impl ASTVisitor<LocalAST> for FunctionLocalizer {
             f: Rc::new(self.visit(f)?),
             args: self.multi_visit(args)?,
         })
+    }
+}
+
+pub use self::visitors::*;
+
+mod visitors {
+    use super::GlobalDef;
+    use super::LocalAST;
+    use super::LocalDef;
+    use super::LocalLiftedAST;
+    use data::Keyword;
+    use data::Literal;
+    use errors::*;
+    use std::rc::Rc;
+
+    /// Traverse a LocalAST, optionally producing a value alongside errors.
+    pub trait LocalASTVisitor<R> {
+        /// Dispatch a `LocalAST`, and add error context.
+        ///
+        /// This doesn't recurse itself, but relies on implementations
+        /// to call `visit` again as necessary.
+        fn visit(&mut self, expr: &LocalAST) -> Result<R> {
+            let r = match expr {
+                LocalAST::Value(l) => self.value_expr(l).context("Visiting value expr"),
+                LocalAST::If { pred, then, els } => {
+                    self.if_expr(pred, then, els).context("Visiting if expr")
+                }
+                LocalAST::Def(def) => self.def_expr(def).context("Visiting def expr"),
+                LocalAST::LocalDef(def) => {
+                    self.localdef_expr(def).context("Visiting localdef expr")
+                }
+                LocalAST::Let { defs, body } => {
+                    self.let_expr(defs, body).context("Visiting let expr")
+                }
+                LocalAST::Do(asts) => self.do_expr(&asts).context("Visiting do expr"),
+                LocalAST::GlobalVar(k) => self.globalvar_expr(k).context("Vising globalvar expr"),
+                LocalAST::LocalVar(i) => self.localvar_expr(*i).context("Vising localvar expr"),
+                LocalAST::Application { f, args } => self
+                    .application_expr(f, args)
+                    .context("Visiting application expr"),
+            }?;
+
+            Ok(r)
+        }
+
+        /// Visit multiple asts, tagging each result with indexed context, and collecting it into a result.
+        fn multi_visit(&mut self, exprs: &[LocalAST]) -> Result<Vec<R>> {
+            let rs: Vec<R> = exprs
+                .iter()
+                .enumerate()
+                .map(|(i, ast)| {
+                    let a = self
+                        .visit(ast)
+                        .context(format!("While visiting multi expression {:}", i))?;
+                    Ok(a)
+                })
+                .collect::<Result<_>>()?;
+
+            Ok(rs)
+        }
+
+        /// Callback for `LocalAST::Value`, passing a reference to the contained literal.
+        fn value_expr(&mut self, l: &Literal) -> Result<R>;
+
+        /// Callback for `LocalAST::If`, passing in the predicate and 2 branches.
+        fn if_expr(
+            &mut self,
+            pred: &Rc<LocalAST>,
+            then: &Rc<LocalAST>,
+            els: &Rc<LocalAST>,
+        ) -> Result<R>;
+
+        /// Callback for `LocalAST::Def`, passing in a reference to the `GlobalDef`.
+        fn def_expr(&mut self, def: &Rc<GlobalDef>) -> Result<R>;
+
+        /// Callback for `LocalAST::LocalDef`, passing in a reference to the `LocalDef`.
+        fn localdef_expr(&mut self, def: &Rc<LocalDef>) -> Result<R>;
+
+        /// Callback for `LocalAST::Let`, passing in a slice of `LocalDef` and a reference to the body.
+        fn let_expr(&mut self, defs: &[LocalDef], body: &LocalAST) -> Result<R>;
+
+        /// Callback for `LocalAST::Do`, passing in a slice of `LocalAST`.
+        fn do_expr(&mut self, exprs: &[LocalAST]) -> Result<R>;
+
+        /// Callback for `LocalAST::GlobalVar`, passing in a reference to the name.
+        fn globalvar_expr(&mut self, name: &Keyword) -> Result<R>;
+
+        /// Callback for `LocalAST::LocalVar`, passing the index directly.
+        fn localvar_expr(&mut self, index: usize) -> Result<R>;
+
+        /// Callback for `LocalAST::Application`, passing in the function and a slice of the arguments.
+        fn application_expr(&mut self, f: &Rc<LocalAST>, args: &[LocalAST]) -> Result<R>;
+    }
+
+    /// Traverse one or multiple `LocalDef`s, tagging the results with context.
+    pub trait LocalDefVisitor<R> {
+        /// Visit multiple `LocalDef`s, collecting the result in a `Vec`.
+        fn visit_multi_localdef(&mut self, defs: &[LocalDef]) -> Result<Vec<R>> {
+            let rs: Vec<R> = defs
+                .iter()
+                .enumerate()
+                .map(|(i, def)| {
+                    let a = self
+                        .visit_localdef(def.name, &def.value)
+                        .context(format!("While parsing def #{:}", i))?;
+                    Ok(a)
+                })
+                .collect::<Result<_>>()?;
+
+            Ok(rs)
+        }
+
+        /// Visit a single `LocalDef`.
+        ///
+        /// This atuomatically destructures the `LocalDef`, and tags the result with context.
+        fn visit_single_localdef(&mut self, d: &LocalDef) -> Result<R> {
+            let res = self
+                .visit_localdef(d.name, &d.value)
+                .context(format!("While visiting localdef {:}", d.name))?;
+            Ok(res)
+        }
+
+        /// Callback for a single `LocalDef`, passing in the name and value `LocalAST`.
+        fn visit_localdef(&mut self, index: usize, value: &LocalAST) -> Result<R>;
+    }
+
+    /// Traverse one or multiple `GlobalDef`s, tagging the results with context.
+    pub trait GlobalDefVisitor<R> {
+        /// Visit multiple `GlobalDef`s, collecting the result in a `Vec`.
+        fn visit_multi_globaldef(&mut self, defs: &[GlobalDef]) -> Result<Vec<R>> {
+            let rs: Vec<R> = defs
+                .iter()
+                .enumerate()
+                .map(|(i, def)| {
+                    let a = self
+                        .visit_globaldef(&def.name, &def.value)
+                        .context(format!("While parsing def #{:}", i))?;
+                    Ok(a)
+                })
+                .collect::<Result<_>>()?;
+
+            Ok(rs)
+        }
+
+        /// Visit a single `GlobalDef`.
+        ///
+        /// This atuomatically destructures the `GlobalDef`, and tags the result with context.
+        fn visit_single_localdef(&mut self, d: &GlobalDef) -> Result<R> {
+            let res = self
+                .visit_globaldef(&d.name, &d.value)
+                .context(format!("While visiting localdef {:}", d.name))?;
+            Ok(res)
+        }
+
+        /// Callback for a single `GlobalDef`, passing in the name and value `LocalAST`.
+        fn visit_globaldef(&mut self, name: &Keyword, value: &LocalAST) -> Result<R>;
+    }
+
+    /// Traverse a `LocalLiftedAST`, with error context tagging.
+    pub trait LLASTVisitor<R> {
+        /// Visit all the functions in a `LocalLiftedAST`, tagging them with their index.
+        fn llast_visit(&mut self, llast: &LocalLiftedAST) -> Result<Vec<R>> {
+            let entry = llast.entry;
+
+            let rs = llast
+                .functions
+                .iter()
+                .enumerate()
+                .map(|(idx, func)| {
+                    let res = self
+                        .visit_local_function(&func.args, &func.body, idx == entry)
+                        .context(format!("While visiting function {:}", idx))?;
+
+                    Ok(res)
+                })
+                .collect::<Result<_>>()
+                .context("While visiting LocalLiftedAST")?;
+
+            Ok(rs)
+        }
+
+        /// Visit a local function, passing in references to the arguments, body, and whether this function is the entry.
+        fn visit_local_function(
+            &mut self,
+            args: &[Keyword],
+            body: &Rc<LocalAST>,
+            entry: bool,
+        ) -> Result<R>;
     }
 }
 
