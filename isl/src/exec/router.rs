45c189111b54f53987be65214fb05b8be0fefd68
Andrew Amis <atamiser@gmail.com>
2020-04-16 00:22:50 +0200

    Fix merge error

--- isl/src/exec/router.rs
+++ isl/src/exec/router.rs
@@ -50,6 +50,7 @@ impl Router {
             rx,
             queue: VecDeque::new(),
             state: RouterState::new(),
+            watches: DiGraphMap::new(),
             quitting: false,
             debug: false,
         }
