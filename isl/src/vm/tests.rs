aeac3b9de7f0182e34ee62e30a7dbd0165cfa012
Andrew Amis <atamiser@gmail.com>
2020-04-18 15:44:14 +0200

    Cargo fmt

--- isl/src/vm/tests.rs
+++ isl/src/vm/tests.rs
@@ -446,10 +446,12 @@ fn test_load_local() {
     assert_eq!(*vm.stack.last().unwrap(), Literal::from(2));
 }
 
-
 #[test]
 fn test_literal_pool() {
-    let mut vm = VM::new(Bytecode::with_pool(vec![], vec![Literal::Number(1), Literal::Number(2)]));
+    let mut vm = VM::new(Bytecode::with_pool(
+        vec![],
+        vec![Literal::Number(1), Literal::Number(2)],
+    ));
 
     vm.op_load_pool(0).unwrap();
 
